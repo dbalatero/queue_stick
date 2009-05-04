13ea233580d33eeb050cb9cf948495840d9c7af9
David Balatero <dbalatero@gmail.com>
2009-05-04 16:29:59 -0700

    Initial commit to queue-tip.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'queue_tip'
+
+Spec::Runner.configure do |config|
+  
+end
