4fc1e110ca8bb4755439d1a383b370f87d1cd98d
David Balatero <dbalatero@gmail.com>
2009-05-14 23:07:07 -0700

    Added web_server skeleton.

--- lib/queue_tip.rb
+++ lib/queue_tip.rb
@@ -1,7 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'queue_tip/counter'
 require 'queue_tip/window_counter'
+
 require 'queue_tip/runner'
+require 'queue_tip/web_server'
 require 'queue_tip/worker'
 
 module QueueTip
---
+++ lib/queue_tip/web_server.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'sinatra/base'
+
+module QueueTip
+  class WebServer < Sinatra::Application
+  end
+end
