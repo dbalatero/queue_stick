6df2face526bc27982a9512a966ee34960a85b22
David Balatero <dbalatero@gmail.com>
2009-05-26 17:34:58 -0700

    Added base methods for process() and recover()

--- lib/queue_tip/worker.rb
+++ lib/queue_tip/worker.rb
@@ -1,5 +1,13 @@
 module QueueTip
   class Worker
+    def process(message)
+      raise NotImplementedError, "Your worker class needs to implement def process(message)!"
+    end
+
+    def recover
+      raise NotImplementedError, "Your worker class needs to implement def recover()!"
+    end
+
     def self.queue_name(name = nil)
       if name.nil?
         @@queue_name
--- spec/queue_tip/worker_spec.rb
+++ spec/queue_tip/worker_spec.rb
@@ -6,5 +6,34 @@ describe QueueTip::Worker do
       QueueTip::Worker.queue_name(:my_queue_name)
       QueueTip::Worker.queue_name.should == :my_queue_name
     end
+
+    it "should not overwrite each subclass' queue name" do
+      class WorkerA < QueueTip::Worker; end
+      class WorkerB < QueueTip::Worker; end
+
+      WorkerA.queue_name(:worker_a_queue)
+      WorkerB.queue_name(:worker_b_queue)
+
+      WorkerA.queue_name.should == :worker_a_queue
+      WorkerB.queue_name.should == :worker_b_queue
+    end
+  end
+
+  describe "process" do
+    it "should raise NotImplementedError at the base class" do
+      lambda {
+        worker = QueueTip::Worker.new
+        worker.process("my message")
+      }.should raise_error(NotImplementedError)
+    end
+  end
+
+  describe "recover" do
+    it "should raise NotImplementedError at the base class" do
+      lambda {
+        worker = QueueTip::Worker.new
+        worker.recover
+      }.should raise_error(NotImplementedError)
+    end
   end
 end
