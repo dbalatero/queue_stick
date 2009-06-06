557f6d75cac6090b57e23b3bd3cc9252a6b054a6
David Balatero <dbalatero@gmail.com>
2009-06-05 18:16:13 -0700

    Don't register trap(:INT) on specs, and Sinatra should be in test mode.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,14 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'queue_stick'
 
+# Don't register trap(:INT) for our specs.
+$DISABLE_TRAPS = true
+$SINATRA_ENV = 'test'
+
 require 'spec/interop/test'
 require 'rack/test'
 
 Test::Unit::TestCase.send(:include, Rack::Test::Methods)
 
 Spec::Runner.configure do |config|
-  
 end
