479e69925f1add7d9c2880ea01bbf72f9a4a824b
David Balatero <dbalatero@gmail.com>
2009-06-09 21:51:06 -0700

    Updated the echo worker to raise an error 15% of the time, so that we can test errors out on the web server.

--- examples/simple_mock_worker/echo_worker.rb
+++ examples/simple_mock_worker/echo_worker.rb
@@ -2,8 +2,13 @@ require File.dirname(__FILE__) + '/../../lib/queue_stick'
 
 class EchoWorker < QueueStick::MockWorker
   def process(message)
-    puts message
-    sleep 2
+    if rand < 0.15
+      # 15% chance of raising an error!
+      raise ArgumentError, "We got an error processing this message!"
+    else
+      puts message
+      sleep 2
+    end
   end
 
   def recover
