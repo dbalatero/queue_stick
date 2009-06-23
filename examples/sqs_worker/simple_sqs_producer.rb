9eea596b6dc48e81ec755e02393d916b0139dfc8
David Balatero <dbalatero@gmail.com>
2009-06-23 07:59:45 -0700

    Changed from #count to #size for RCR.

--- examples/sqs_worker/simple_sqs_producer.rb
+++ examples/sqs_worker/simple_sqs_producer.rb
@@ -3,8 +3,8 @@
 require 'rubygems'
 require 'right_aws'
 
-sqs = RightAws::SqsGen2.new('<your access key>',
-                            '<your secret key>',
+sqs = RightAws::SqsGen2.new('1N2G2X8M88XC4KTYRHG2',
+                            'yQHr7PhLOfBkpnaK1MbPBcDxKKO6snAW500f3Lyl',
                             :multi_thread => false,
                             :signature_version => '1')
 queue = sqs.queue('my_test_queue', true)
--- examples/sqs_worker/simple_sqs_worker.rb
+++ examples/sqs_worker/simple_sqs_worker.rb
@@ -1,8 +1,8 @@
 require File.dirname(__FILE__) + '/../../lib/queue_stick'
 
 class SimpleSQSWorker < QueueStick::SQSWorker
-  aws_access_key_id '<your key here>'
-  aws_secret_access_key '<your secret here>'
+  aws_access_key_id '1N2G2X8M88XC4KTYRHG2'
+  aws_secret_access_key 'yQHr7PhLOfBkpnaK1MbPBcDxKKO6snAW500f3Lyl'
   queue_name 'my_test_queue'
   visibility_timeout 120
 
